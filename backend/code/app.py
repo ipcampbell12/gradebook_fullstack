649a14a62fe0d97850b9c98f8391fc97e9010293
ipcampbell12 <ipcampbell12@gmail.com>
2022-12-14 10:50:23 -0800

    class config

--- backend/code/app.py
+++ backend/code/app.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config.from_pyfile('../config.py')
+app.config.from_object('config.Config')
 db = SQLAlchemy(app)
 
 
--- backend/config.py
+++ backend/config.py
@@ -1,9 +1,25 @@
-SECRET_KEY = "Ian"
-DEBUG = True
-# Sets PROPAGATE_EXCEPTIONS to implicitly true
+
+from os import environ, path
+from dotenv import load_dotenv
 
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:///boardgames.db'
-SQLALCHEMY_TRACK_MODIFICATIONS = False
+basedir = path.abspath(path.dirname(__file__))
+load_dotenv(path.join(basedir, '.env'))
 
-# should be stored in instance folder?
+
+# Base configuration
+class Config:
+    SECRET_KEY = environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = environ.get('SQLALCHEMY_DATABASE_URI')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+# HEY CHECK IT OUT! THAT'S SOME CLASS INHERITANCE!
+class ProdConfig(Config):
+    FLASK_ENV = 'production'
+    DEBUG = False
+
+
+class DevConfig(Config):
+    FLASK_ENV = 'development'
+    DEBUG = True
