1a5fe4312c96660a2638dc0b7a390f98e6673212
ipcampbell12 <ipcampbell12@gmail.com>
2022-12-14 10:08:30 -0800

    config.py

--- backend/code/app.py
+++ backend/code/app.py
@@ -2,11 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.secret_key = "Ian"
-
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///boardgames.db'
-#engine = create_engine('sqlite:///boardgames.db')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config.from_pyfile('config.py')
 db = SQLAlchemy(app)
 
 
---
+++ backend/config.py
@@ -0,0 +1,9 @@
+SECRET_KEY = "Ian"
+DEBUG = True
+# Sets PROPAGATE_EXCEPTIONS to implicitly true
+
+
+SQLALCHEMY_DATABASE_URI = 'sqlite:///boardgames.db'
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+# should be stored in instance folder?
